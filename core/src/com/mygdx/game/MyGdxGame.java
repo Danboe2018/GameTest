a2c172de70815843abafaf65fb2855d74ebf204d
Dan Boehmke <Danjboehmke@gmail.com>
2019-10-12 18:31:31 -0400

    Adds animation

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,25 +2,25 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter {
 
 	private SpriteBatch batch;
-	private Texture img;
-	private Sprite sprite;
+	private TextureAtlas runAtlas;
+	private Animation<TextureRegion> animation;
+	private float timePassed = 0;
+
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("flower.jpg");
-		sprite = new Sprite(img);
-
+		runAtlas = new TextureAtlas(Gdx.files.internal("run.atlas"));
+		animation = new Animation(1/15f,runAtlas.getRegions());
 	}
 
 	@Override
@@ -29,9 +29,8 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		batch.begin();
-
-		sprite.draw(batch);
-
+		timePassed += Gdx.graphics.getDeltaTime();
+		batch.draw(animation.getKeyFrame(timePassed,true),300,300);
 		batch.end();
 
 	}
@@ -39,6 +38,6 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void dispose() {
 		batch.dispose();
-		img.dispose();
+		runAtlas.dispose();
 	}
 }
