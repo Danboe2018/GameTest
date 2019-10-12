d6c3c6ec269c3d718f2ae1d2b61fa45b0e8db06c
Dan Boehmke <Danjboehmke@gmail.com>
2019-10-12 18:42:16 -0400

    Sets up gestures

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,23 +4,17 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Vector2;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends ApplicationAdapter implements GestureDetector.GestureListener {
 
 	private SpriteBatch batch;
-	private TextureAtlas runAtlas;
-	private Animation<TextureRegion> animation;
-	private float timePassed = 0;
 
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		runAtlas = new TextureAtlas(Gdx.files.internal("run.atlas"));
-		animation = new Animation(1/15f,runAtlas.getRegions());
 	}
 
 	@Override
@@ -29,8 +23,7 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		batch.begin();
-		timePassed += Gdx.graphics.getDeltaTime();
-		batch.draw(animation.getKeyFrame(timePassed,true),300,300);
+
 		batch.end();
 
 	}
@@ -38,6 +31,52 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void dispose() {
 		batch.dispose();
-		runAtlas.dispose();
+	}
+
+	@Override
+	public boolean pan(float x, float y, float deltaX, float deltaY) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(float x, float y, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean tap(float x, float y, int count, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean longPress(float x, float y) {
+		return false;
+	}
+
+	@Override
+	public boolean fling(float velocityX, float velocityY, int button) {
+		return false;
+	}
+
+
+
+	@Override
+	public boolean panStop(float x, float y, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean zoom(float initialDistance, float distance) {
+		return false;
+	}
+
+	@Override
+	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+		return false;
+	}
+
+	@Override
+	public void pinchStop() {
+
 	}
 }
